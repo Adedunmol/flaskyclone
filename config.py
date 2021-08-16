0dd9bd194531c263d63fd4c345f560652f227526
Adedunmol <oyewaleadedunmola@gmail.com>
2021-08-16 20:48:18 +0100

    changed env variables of mail username and password

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ class Config:
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
                             ['true', 'on', '1']
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = 'oyewaleadedunmola@gmail.com'
+    MAIL_PASSWORD = 'Geek@2004'
     FLASKY_MAIL_SUBJECT_PREFIX = '[FLASKY]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <oyewaleadedunmola@gmail.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
