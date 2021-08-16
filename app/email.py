11a49d97651ec2651d5b222542058677c906adc7
Adedunmol <oyewaleadedunmola@gmail.com>
2021-08-16 21:26:19 +0100

    added 500.html and app into send_mail function

--- app/email.py
+++ app/email.py
@@ -9,11 +9,11 @@ def send_async_mail(app, msg):
         mail.send(msg)
 
 def send_mail(to, subject, template, **kwargs):
-
+    app = current_app._get_current_object()
     msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
                     sender=current_app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_mail, args=[current_app, msg])
+    thr = Thread(target=send_async_mail, args=[app, msg])
     thr.start()
     return thr
