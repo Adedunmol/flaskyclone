eed11fe0ffc31c35c438dce58744b8e7c6690739
Adedunmol <oyewaleadedunmola@gmail.com>
2021-08-16 13:02:34 +0100

    uncommented send_mail during registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -40,8 +40,8 @@ def register():
         db.session.add(user)
         db.session.commit()
         token = user.generate_confirmation_token()
-        #send_mail(user.email, 'Confirm Your Account', 'auth/email/confirm', user=user, token=token)
-        #flash('A confirmation email has been sent to you by email.')
+        send_mail(user.email, 'Confirm Your Account', 'auth/email/confirm', user=user, token=token)
+        flash('A confirmation email has been sent to you by email.')
         return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
 
