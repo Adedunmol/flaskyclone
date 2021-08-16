f93be3398756d258947e8ab5e84fc4d5f258732c
Adedunmol <oyewaleadedunmola@gmail.com>
2021-08-16 11:37:46 +0100

    added sqlite database as option

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     uri = os.environ.get('DATABASE_URL')
     if uri.startswith('postgres://'):
-        SQLALCHEMY_DATABASE_URI = uri.replace('postgres://', 'postgresql://', 1)
+        SQLALCHEMY_DATABASE_URI = uri.replace('postgres://', 'postgresql://', 1) or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
     @classmethod
     def init_app(cls, app):
