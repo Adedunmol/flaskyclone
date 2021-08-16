e729891c11d8e5e6d7931e4f097f3fe58526456a
Adedunmol <oyewaleadedunmola@gmail.com>
2021-08-16 21:16:04 +0100

    changed the email function back

--- app/email.py
+++ app/email.py
@@ -4,18 +4,16 @@ from flask import render_template, current_app
 from threading import Thread
 
 
-#def send_async_mail(app, msg):
-#    with app.app_context():
-#        mail.send(msg)
+def send_async_mail(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_mail(to, subject, template, **kwargs):
-    app = current_app
-    with app.app_context():
 
-        msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+    msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
                     sender=current_app.config['FLASKY_MAIL_SENDER'], recipients=[to])
-        msg.body = render_template(template + '.txt', **kwargs)
-        msg.html = render_template(template + '.html', **kwargs)
-        thr = Thread(target=mail.send(msg))
-        thr.start()
-        return thr
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_mail, args=[current_app, msg])
+    thr.start()
+    return thr
